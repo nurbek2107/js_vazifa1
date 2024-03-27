166bef281a552c65acaf45e4c3acda976ca89f39
nurbek2107 <abduaxatov002@gmail.com>
2024-03-28 00:47:49 +0500

    feat:js

---
+++ main.js
@@ -0,0 +1,14 @@
+let i = prompt("ismingizni kiriting")
+let a = prompt("summani kiriting")
+let yolkira = 500
+let mehmanhona = 250
+let muzey = 120 
+let dollor = 11000.34 
+let yevro =12354.03
+let yigindi = (yolkira * dollor) + (mehmanhona * dollor) + (muzey * yevro)
+if (a >= yigindi){
+    alert('OQ yol, '+i)
+}  
+else {
+alert(i+' ,Ozgina sabr qilish kerak bolar ekan')
+}
