2c6ec2fac17958081a225bdb02b786d772321348
nurbek2107 <abduaxatov002@gmail.com>
2024-03-28 13:24:04 +0500

    rof:main js

---
+++ 2-masala/main.js
@@ -0,0 +1,23 @@
+let a = prompt("1 dan 7 gacha son kiriting")
+if (a == 1){
+    alert('Dushanba')
+}  
+else if(a == 2){
+    alert('Seshanba')
+}
+else if(a == 3){
+    alert('Chorshanba')
+}
+else if(a == 4){
+    alert('Payshanba')
+}
+else if(a == 5){
+    alert('Juma')
+}else if(a == 6){
+    alert('Shanba')
+}else if(a == 7){
+    alert('Yakshanba')
+}
+else {
+alert('Hato son kirittiz')
+}
